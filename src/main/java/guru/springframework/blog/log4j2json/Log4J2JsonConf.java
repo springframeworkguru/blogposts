dfbc3446a34c242a36de35bcd867308c976ba635
Ximanta <ximanta.sarma@gmail.com>
2016-03-17 17:59:28 +0530

    Added code example for Log4J2 JSON configuration.

---
+++ src/main/java/guru/springframework/blog/log4j2json/Log4J2JsonConf.java
@@ -0,0 +1,17 @@
+package guru.springframework.blog.log4j2json;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4J2JsonConf {
+    private static Logger logger = LogManager.getLogger();
+
+    public void performSomeTask(){
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        logger.fatal("This is a fatal message");
+    }
+}
---
+++ src/test/java/guru/springframework/blog/log4j2json/Log4J2JsonConfTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.blog.log4j2json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Log4J2JsonConfTest {
+    @Test
+    public void testPerformSomeTask() throws Exception {
+        Log4J2JsonConf log4J2JsonConf=new Log4J2JsonConf();
+        log4J2JsonConf.performSomeTask();
+
+    }
+}

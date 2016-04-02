0d2c900bbe7edecf36992c6e6abdb68c27f524cd
Ximanta <ximanta.sarma@gmail.com>
2016-04-02 12:50:05 +0530

    Deleted non-web applications Added Web-based code example for Using Log4J 2 with Spring Boot

---
+++ src/main/java/guru/springframework/blog/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package guru.springframework.blog.controllers;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @RequestMapping("/")
+    String index(){
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        return "index";
+    }
+}

1a2daf21a96f8cf60deb011ec471130ce2d46b68
Ximanta <ximanta.sarma@gmail.com>
2016-04-12 17:18:33 +0530

    Added code example for Logback XML Configuration.

---
+++ src/main/java/guru/springframework/blog/logbackxml/LogbackConfigXml.java
@@ -0,0 +1,15 @@
+package guru.springframework.blog.logbackxml;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogbackConfigXml {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    public void performTask(){
+        logger.debug("This is a debug message.");
+        logger.info("This is an info message.");
+        logger.warn("This is a warn message.");
+        logger.error("This is an error message.");
+
+    }
+}

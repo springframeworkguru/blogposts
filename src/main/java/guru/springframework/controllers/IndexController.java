d3147f77964c7d97bf9415ab0428ed52bef63b9e
Ximanta <ximanta.sarma@gmail.com>
2016-04-20 21:16:35 +0530

    Added code example for logback configurtion with springboot

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,22 @@
+package guru.springframework.controllers;
+
+
+import guru.springframework.helpers.SpringLoggingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @RequestMapping("/")
+    String index(){
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
+        new SpringLoggingHelper().helpMethod();
+        return "index";
+    }
+}

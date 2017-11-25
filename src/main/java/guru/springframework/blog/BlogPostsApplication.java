f64b9ca6d634e6aa41a6966a0323afd02be952b4
Ximanta <ximanta.sarma@gmail.com>
2017-11-25 19:15:36 +0530

    Initial commit "Jackson Pretty Print"

--- src/main/java/guru/springframework/blog/BlogPostsApplication.java
+++ src/main/java/guru/springframework/blog/BlogPostsApplication.java
@@ -2,8 +2,11 @@ package guru.springframework.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
 public class BlogPostsApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/guru/springframework/blog/jsonprettyprinting/domain/JsonPrettyPrintDemoBeanTest.java
@@ -0,0 +1,67 @@
+package guru.springframework.blog.jsonprettyprinting.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import guru.springframework.blog.jsonprettyprinting.domain.JsonPrettyPrintDemoBean;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class JsonPrettyPrintDemoBeanTest {
+    private ObjectMapper objectMapper;
+    @Before
+    public void setUp() throws Exception{
+        objectMapper = new ObjectMapper();
+    }
+    @After
+    public void tearDown() throws Exception{
+        objectMapper = null;
+    }
+    @Test
+    public void testJsonPrettyPrintObject() throws JsonProcessingException {
+        /*JSON without pretty print*/
+        String jsonCompactString = objectMapper.writeValueAsString(new JsonPrettyPrintDemoBean());
+        System.out.println("JSON without pretty print");
+        System.out.println(jsonCompactString);
+        assertThat(jsonCompactString, containsString("person-id"));
+        assertThat(jsonCompactString, containsString("person-name"));
+        /*JSON with pretty print*/
+        String jsonString = objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(new JsonPrettyPrintDemoBean());
+        System.out.println("JSON with pretty print");
+        System.out.println(jsonString);
+        assertThat(jsonString, containsString("person-id"));
+        assertThat(jsonString, containsString("person-name"));
+    }
+    @Test
+    public void testJsonPrettyPrintString() throws IOException {
+        String jsonString = "{\"person-id\": 231, \"person-name\": \"Mary Parker\"}";
+        /*JSON output in compact mode:*/
+        System.out.println("JSON without mapping to object");
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(jsonString));
+        /*Reading into POJO and pretty print*/
+        System.out.println("JSON  mapping to POJO with pretty print");
+        JsonPrettyPrintDemoBean bean = objectMapper
+                .readValue(jsonString, JsonPrettyPrintDemoBean.class);
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(bean));
+        assertThat(bean.personName, is(equalTo("Mary Parker")));
+        assertThat(bean.personId, is(equalTo(231L)));
+        /*Reading into Object and pretty print*/
+       Object object = objectMapper
+               .readValue(jsonString, Object.class);
+        System.out.println("JSON  mapping to Object with pretty print");
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(object));
+
+    }
+}
